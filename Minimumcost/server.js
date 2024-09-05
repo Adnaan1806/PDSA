0f32160d9a3994b69c537d94a59646af1341cfaa
Adnaan Janees <adnaanjanees0@gmail.com>
2024-09-05 22:40:28 +0530

    minor changes

--- Minimumcost/server.js
+++ Minimumcost/server.js
@@ -31,6 +31,6 @@ app.post("/saveResult", (req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log("Server is running on port 3000");
 });
